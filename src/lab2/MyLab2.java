0542c3d539027fafb2613cbda1cf039b06ad2bcb
Nastyalaaaadina <nastyaladina@yandex.ru>
2021-11-22 17:08:26 +0300

    'first commit'

---
+++ src/lab2/MyLab2.java
@@ -0,0 +1,29 @@
+package lab2;
+
+import java.util.Scanner;
+
+public class MyLab2 {
+
+    static int fact(int n){
+            int result =1;
+            while (n!=0){
+                result *= n;
+                n--;}
+            return result;}
+        public static void main (String [] args) {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Введите число от 0 до 14:");
+            int n = scan.nextInt();
+            for (int i=0;i<1;i++)
+                if (n>-1 & n<15) {
+                    int result = fact(n);
+                    System.out.println("Факториал от " + n + " равен: " + result);}
+            if (n>=15){
+                System.out.println(" Вы ввели слишком большое число, попробуйте еще раз");
+            }
+            if (n<0){
+                System.out.println(" Число, введенное вами не соответсвует требованиям");
+            }
+        }
+    }
+
